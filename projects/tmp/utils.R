1847111f5e884a64ec451f3b244268d42f80e18d
Benjamin Peter <bpeter@uchicago.edu>
2016-12-05 17:51:45 -0600

    functions to transform from category to id

--- projects/tmp/utils.R
+++ projects/tmp/utils.R
@@ -1,10 +1,11 @@
-
 NCAT <- 4
 NPOP <- 5
+BASE <- NCAT^(0:(NPOP-1))
+
 cat2id <- function(cat){
   #print(cat)
   if(is.null(dim(cat))){
-    sum(cat * NCAT^(0:(NPOP-1)))
+    sum(cat * BASE)
   }else{
     apply(cat, 1, cat2id)
   }
@@ -16,9 +17,9 @@ id2cat <- function(id){
   if(length(id)== 1){
   cat <- rep(0, NPOP)
   for(i in NPOP:1){
-    cat[i] <- id %/% 4^(i-1)
+    cat[i] <- id %/% BASE[i]
     #print(id)
-    id <- id - cat[i] * 4^(i-1)
+    id <- id - cat[i] * BASE[i]
   }
     return(cat)
   } else {
